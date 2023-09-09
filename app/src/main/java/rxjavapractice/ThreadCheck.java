8622e6a8c79e59adc5bcbb84a8cbc993b7e9eb0e
HanaokaY <okayu5757@ezweb.ne.jp>
2023-09-09 16:59:01 +0900

    異なるスレッドで実行されていることをテスト

---
+++ app/src/main/java/rxjavapractice/ThreadCheck.java
@@ -0,0 +1,31 @@
+package rxjavapractice;
+
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+public class ThreadCheck {
+    public static void main(String[] args) throws InterruptedException {
+        Observable<String> observable = Observable.create(emitter -> {
+            System.out.println(" エミットしたスレッド =>  " + Thread.currentThread().getName());
+            emitter.onNext("Hello");
+            emitter.onNext("World");
+            emitter.onComplete();
+        });
+
+        observable
+                .observeOn(Schedulers.io())
+                .subscribe(
+                        item -> {
+                            System.out.println("受け取ったデータ => " + item + ": スレッド => " + Thread.currentThread().getName());
+                        },
+                        throwable -> {
+                            System.err.println("Error  スレッド => " + Thread.currentThread().getName());
+                        },
+                        () -> {
+                            System.out.println("完了したスレッド => " + Thread.currentThread().getName());
+                        }
+                );
+
+        Thread.sleep(1000);
+    }
+}
